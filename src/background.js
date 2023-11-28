37dea81c5e44d93ff8ef37c691f6f703be1b7b20
GuoJikun <qwerboy0912@gmail.com>
2023-11-28 11:51:21 +0800

    ✨ feat: 增加动态注入脚本功能

--- src/background.js
+++ src/background.js
@@ -19,3 +19,41 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
     info: info,
   });
 });
+
+// 监听按键事件
+// chrome.scripting.onMessage.addListener((message, sender, sendResponse) => {
+//   if (message.type === "keydown") {
+//     console.log("Key pressed:", message.key);
+//     // 在这里执行你想要的操作
+//   }
+// });
+
+// 注入到页面的脚本
+function injectScript() {
+  document.addEventListener("keydown", (event) => {
+    console.log("Key pressed:", event.key);
+    // chrome.runtime.sendMessage({ type: "keydown", key: event.key });
+  });
+}
+// 注入脚本到所有标签页
+chrome.tabs.query({}, (tabs) => {
+  tabs.forEach((tab) => {
+    const url = new URL(tab.url);
+    if (!["https:", "http:"].includes(url.protocol)) return;
+    console.log("tab", tab.title, ":", tab.id, tab);
+    chrome.scripting.executeScript(
+      {
+        target: { tabId: tab.id, allFrames: false },
+        function: injectScript,
+      },
+      (result) => {
+        console.log("result", result);
+        if (chrome.runtime.lastError) {
+          console.error(chrome.runtime.lastError);
+        } else {
+          console.log("Script injected successfully");
+        }
+      }
+    );
+  });
+});
